3ccf97bf514db11e46a31162de266d6ca3fd8f67
bangint <bangint.cba.18@gmail.com>
2022-07-30 20:43:55 -0300

    1.3

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { SobreDeComponent } from './componentes/sobre-de/sobre-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
---
+++ portafolio/src/app/componentes/sobre-de/sobre-de.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-de',
+  templateUrl: './sobre-de.component.html',
+  styleUrls: ['./sobre-de.component.css']
+})
+export class SobreDeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
